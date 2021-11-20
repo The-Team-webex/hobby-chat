910d4befc8229a462553d03e918c162c7305af30
mi-TSUKASA <mi.tsukasa0898@gmail.com>
2021-11-20 20:38:33 +0900

    設定完了

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOGbWI-p5fQNpRYSqYZN0UrYRCQ7SXQWA",
+  authDomain: "hobbychat-fd9e4.firebaseapp.com",
+  projectId: "hobbychat-fd9e4",
+  storageBucket: "hobbychat-fd9e4.appspot.com",
+  messagingSenderId: "127206945586",
+  appId: "1:127206945586:web:7abdeb904b48ccd09f9d2b",
+  measurementId: "G-D9DMXPK1L3",
+}
+
+firebase.initializeApp(firebaseConfig)
