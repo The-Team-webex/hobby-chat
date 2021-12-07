c0cf4e42dcbfa2bd0714fb7b967ee8168cbdeb91
정재희 <jaeheechung@jeongjaehuis-MacBook-Pro.local>
2021-12-07 22:16:26 +0900

    conflictあとのエラーを修正

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -102,45 +102,11 @@ export default {
       // })
     })
   },
-            if (snapshot.exists) {
-              this.$store.state.userData.name = snapshot.data().name
-              this.$store.state.userData.college = snapshot.data().college
-            } else {
-              const userData = {
-                id: user.uid,
-                name: user.displayName,
-                photo: user.photoURL,
-                college: "未設定",
-              }
-              firebase
-                .firestore()
-                .collection("users")
-                .doc(user.uid)
-                .set(userData)
-              this.$store.state.userData.name = snapshot.data().name
-              this.$store.state.userData.college = snapshot.data().college
-            }
-          })
-      }
-    })
-
-    firebase.auth().onAuthStateChanged((user) => {
-      // if (user && this.$store.state.userData.id === "") {
-      if (user) {
-        this.$store.state.isAuth = true
-        console.log("ログインしています")
-      } else {
-        this.$store.state.isAuth = false
-        console.log("ログインしていません")
-      }
-    })
-  },
   computed: {
     getAuth: function () {
       return this.$store.state.isAuth
     },
   },
-
   watch: {
     getAuth: function () {
       if (this.$store.state.isAuth === false) {
